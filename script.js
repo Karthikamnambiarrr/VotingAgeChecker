c02951796c617a1cf76301b3da70f3324d2fcc27
unknown <karthikanambiar10@gmail.com>
2024-09-21 11:37:29 +0530

    Your commit message here

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 function ageChecker(){
     let age = document.querySelector("input").value;
     let ageNum = parseInt(age);
+    let text = document.querySelector("#div2");
    if(ageNum>=18){
-    console.log("You are eligible to vote");
-   }
-   else{
-    console.log("You are not eligible to vote")
-   }
+    text.innerHTML=`<br><h3>You are eligible to vote</h3>`;
+    text.style.color="red" ;
+    }
+    else{
+        text.innerHTML=`<br><h3>You are not eligible to vote</h3>`;
+        text.style.color="green" ;
+
+    }
+   
 }
